d62570569117c6d1bc0288bb825109d11225352d
Andrew Christ <andrew@openmarmot.com>
2020-09-07 18:53:12 -0700

    2d math updates

--- code/engine/math_2d.py
+++ code/engine/math_2d.py
@@ -24,14 +24,37 @@ module_last_update_date='July 02 2016' #date of last update
 #global variables
 
 #------------------------------------------------------------------------------
+def get_vector_length(vec2):
+	return math.sqrt(vec2[0]*vec2[0]+vec2[1]*vec2[1])
+
+#------------------------------------------------------------------------------
 def get_rotation(coords, target_coords):
 	delta_x=coords[0]-target_coords[0]
 	delta_y=coords[1]-target_coords[1]
 	return math.atan2(delta_x,delta_y) *180 / math.pi
 
 #------------------------------------------------------------------------------
-
 def get_distance(coords1, coords2):
 	x=coords1[0]-coords2[0]
 	y=coords1[1]-coords2[1]
 	return math.sqrt(x*x+y*y)
+
+#------------------------------------------------------------------------------
+def get_normalized(vec2):
+	l=math.sqrt(vec2[0]*vec2[0]+vec2[1]*vec2[1])
+	b=[0.,0.]
+	try:
+		b=[vec2[0]/l,vec2[1]/l]
+	except ZeroDivisionError:
+		pass
+	return b 
+
+#------------------------------------------------------------------------------
+def moveTowardsTarget(speed,location,destination, time_passed):
+	vec_to_dest=[destination[0]-location[0],destination[1]-location[1]]
+	distance=get_vector_length(vec_to_dest)
+	heading=get_normalized(vec_to_dest)
+	travel_distance=min(distance,time_passed*speed)
+	change=[heading[0]*travel_distance,heading[1]*travel_distance]
+	return [location[0]+change[0],location[1]+change[1]]
+
