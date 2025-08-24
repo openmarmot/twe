31661cfbf149c79b8069d195d684a9d378692900
Andrew <andrew@openmarmot.com>
2025-08-24 09:43:48 -0700

    update

--- code/engine/world_builder.py
+++ code/engine/world_builder.py
@@ -2127,6 +2127,57 @@ def spawn_object(world,world_coords,object_type, spawn):
         z.ai.compatible_vehicles=['german_rso_pak','german_rso']
         z.ai.armor=[5,0,0]
 
+    # this is the base object for the sd.kfz.234 variants. It is not meant to be spawned by itself
+    elif object_type=='german_sd_kfz_234_base':
+        # ref : https://en.wikipedia.org/wiki/Sd.Kfz._234
+        # ref : https://tank-afv.com/ww2/germany/Sd-Kfz-234.php
+        z=WorldObject(world,['sd_kfz_234_chassis_green_camo'],AIVehicle)
+        z.name='Sd.Kfz.234'
+        z.is_vehicle=True
+        z.is_towable=True
+
+        z.ai.vehicle_armor['top']=[5,0,0]
+        z.ai.vehicle_armor['bottom']=[5,0,0]
+        z.ai.vehicle_armor['left']=[8,19,0]
+        z.ai.vehicle_armor['right']=[8,19,0]
+        z.ai.vehicle_armor['front']=[30,55,0]
+        z.ai.vehicle_armor['rear']=[10,46,0]
+        z.ai.passenger_compartment_armor['top']=[5,0,0]
+        z.ai.passenger_compartment_armor['bottom']=[5,0,0]
+        z.ai.passenger_compartment_armor['left']=[8,35,0]
+        z.ai.passenger_compartment_armor['right']=[8,35,0]
+        z.ai.passenger_compartment_armor['front']=[30,40,0]
+        z.ai.passenger_compartment_armor['rear']=[10,38,0]
+        z.ai.max_speed=385.9
+        z.ai.max_offroad_speed=177.6
+        z.ai.rotation_speed=40.
+        z.collision_radius=50
+        z.weight=7800
+        z.drag_coefficient=0.9
+        z.frontal_area=5
+        z.ai.fuel_tanks.append(spawn_object(world,world_coords,"vehicle_fuel_tank",False))
+        z.ai.fuel_tanks[0].volume=114
+        fill_container(world,z.ai.fuel_tanks[0],'gas_80_octane')
+        z.ai.engines.append(spawn_object(world,world_coords,"maybach_hl42_engine",False))
+        z.ai.engines[0].ai.exhaust_position_offset=[75,10]
+        z.ai.batteries.append(spawn_object(world,world_coords,"battery_vehicle_6v",False))
+        z.add_inventory(spawn_object(world,world_coords,"german_fuel_can",False))
+        z.add_inventory(get_random_from_list(world,world_coords,list_medical,False))
+        z.add_inventory(get_random_from_list(world,world_coords,list_consumables,False))
+        z.rotation_angle=float(random.randint(0,359))
+        z.add_inventory(spawn_object(world,world_coords,'radio_feldfu_b',False))
+        if random.randint(0,1)==1:
+            z.add_inventory(spawn_object(world,world_coords,"panzerfaust_100",False))
+        z.ai.min_wheels_per_side_front=1
+        z.ai.min_wheels_per_side_rear=1
+        z.ai.max_wheels=8
+        z.ai.max_spare_wheels=0
+        for b in range(2):
+            z.ai.front_left_wheels.append(spawn_object(world,world_coords,"251_wheel",False))
+            z.ai.front_right_wheels.append(spawn_object(world,world_coords,"251_wheel",False))
+            z.ai.rear_left_wheels.append(spawn_object(world,world_coords,"251_wheel",False))
+            z.ai.rear_right_wheels.append(spawn_object(world,world_coords,"251_wheel",False))
+
     # this is the base object for the sd.kfz.251 variants. It is not meant to be spawned by itself
     elif object_type=='german_sd_kfz_251_base':
         # ref : https://tanks-encyclopedia.com/ww2/nazi_germany/sdkfz-251_hanomag.php
