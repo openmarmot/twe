774ba3596108d505cc7ebd92cfb2c87106d0e5e3
andrew <andrew@openmarmot.com>
2026-05-10 16:37:26 +0000

    update

--- code/twe.py
+++ code/twe.py
@@ -20,8 +20,9 @@ Project Github : https://github.com/openmarmot/twe
 
 #import custom packages
 
-import sys
 import argparse
+import time
+import pygame
 from engine.graphics_2d_pygame import Graphics_2D_Pygame
 
 #global variables
@@ -38,6 +39,8 @@ def run():
     parser = argparse.ArgumentParser()
     parser.add_argument('--quick-battle', nargs=2, metavar=('FACTION', 'OPTION'),
                         help="Start quick battle immediately: FACTION (civilian, german, soviet), OPTION (number)")
+    parser.add_argument('--ai-test', nargs=2, metavar=('FACTION', 'SECONDS'),
+                        help="AI testing mode: auto-run quick battle and exit after SECONDS")
     args = parser.parse_args()
 
     graphic_engine=Graphics_2D_Pygame(screen_size)
@@ -46,13 +49,29 @@ def run():
         faction = args.quick_battle[0]
         option = int(args.quick_battle[1])
         graphic_engine.load_quick_battle(faction, option)
-        
+
+    ai_test_seconds = None
+    ai_test_start_time = None
+    ai_test_grace_period = 2.0
+    if args.ai_test:
+        faction = args.ai_test[0]
+        option = int(args.ai_test[1].split('.')[0])
+        ai_test_seconds = float(args.ai_test[1])
+        graphic_engine.load_quick_battle(faction, option)
+        ai_test_start_time = time.time()
 
     # main game loop
     while graphic_engine.quit is False:
 
         graphic_engine.update()
         graphic_engine.render()
+
+        if ai_test_seconds is not None and time.time() - ai_test_start_time >= ai_test_seconds:
+                graphic_engine.quit = True
+                pygame.event.post(pygame.event.Event(pygame.QUIT))
+
+    if args.ai_test:
+        time.sleep(ai_test_grace_period)
 
 #------------------------------------------------------------------------------
 if __name__ == "__main__":
